b941613a8b867c40b5db124576cd5be295a41f2e
Miihzx <santos.lima.emilly@escola.pr.gov.br>
2025-04-15 11:17:09 -0300

    capitulo 2

---
+++ pythonProject1/Capitulo 2/exemplo2nomesdevariaveis.py
@@ -0,0 +1,10 @@
+
+# variaveis nao podem comecar com numeros
+## 76trombones = 'big parede'
+
+# variaveis nao podem comecar com letras maiuscula
+# more = 100000
+
+# variaveis nao podem ter caracteres especiais
+# tirano o _ underline
+# more@ = 1000
