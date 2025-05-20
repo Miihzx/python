2600674ddc742e031b3dbe2a8607c7f56a0f08bd
miih <santos.lima.emilly@escola.pr.gov.br>
2025-05-20 11:46:39 -0300

    exemplo2valoretipo

---
+++ pythonProject1/capitulo 1/exemplo2valoresetipos.py
@@ -0,0 +1,20 @@
+# tipo inteiro  1, 2, 3 ... -2, -3 ....
+inteiro = 2
+
+print(type(inteiro))
+
+# tipo real ou ponto flutuante float 1.3, 0.6 ... -3.456
+float = 1.1
+
+print(type(float))
+
+#caracteres
+char1 ='a'
+char2 = '2'
+
+print(type(char1))
+
+#String cconjunto de carcteres
+StringNome = "Emilly dos Santos"
+
+print(type(StringNome))
